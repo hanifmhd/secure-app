5bbaa2e278febc2730c539898d87be093d075e97
Muhammad Hanif <muhammadhanif.if@gmail.com>
2021-03-09 13:35:10 +0700

    config for jailbreak detection

--- ios/SecureApp/AppDelegate.m
+++ ios/SecureApp/AppDelegate.m
@@ -3,6 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "JB.h"
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -43,6 +44,31 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  /*
+    List function:
+    BOOL isRunningOnMac => detecting running on MAC
+    BOOL isJb() => detecting jailbreak
+    BOOL isInjectedWithDynamicLibrary() => detecting inject with dynamic library
+    BOOL isDebugged() => detecting it is being debugged
+    BOOL isFromAppStore() => detecting app is from Appstore
+    BOOL isSecurityCheckPassed() => result of all of function above that passed security check, make sure it is implemented in Production
+  */
+  // Jailbreak detection, if it is true, show alert, then exit app
+  if (!isSecurityCheckPassed()) {
+      UIAlertController *alertController = [UIAlertController
+                                            alertControllerWithTitle:@"Jailbroken Device is Detected"
+        message:@"You cannot use this app, bye 👋"
+        preferredStyle:UIAlertControllerStyleAlert];
+      UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Ok"
+                                                              style:UIAlertActionStyleDefault
+                                                            handler:^(UIAlertAction *action) {
+        exit(1);
+      }];
+      [alertController addAction:cancelAction];
+      [self.window.rootViewController presentViewController:alertController animated:YES completion:nil];
+  }
+  
   return YES;
 }
 
