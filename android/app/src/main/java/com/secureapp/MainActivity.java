000f1b6e9adccadcddd36b6253dc9c0ceae0a049
Muhammad Hanif <muhammadhanif.if@gmail.com>
2021-03-09 17:26:12 +0700

    config secure switch app - Android

--- android/app/src/main/java/com/secureapp/MainActivity.java
+++ android/app/src/main/java/com/secureapp/MainActivity.java
@@ -1,9 +1,17 @@
 package com.secureapp;
 
+import android.os.Bundle;
+import android.view.WindowManager;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
 
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+    getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+  }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
